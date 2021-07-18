5af8addd722365ad365bc9fc645178de766aa735
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-07-18 18:57:17 +0200

    Initial Commit

---
+++ PalindromeSwiftUI/PalindromeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalindromeSwiftUIApp.swift
+//  PalindromeSwiftUI
+//
+//  Created by Tomislav Gelesic on 18.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct PalindromeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
